68347411678d3b88089a75c858d0b52947aec446
sunyu11 <sunyu11@jd.com>
2018-11-30 12:10:20 +0800

    新增maven profile配置 修改spring cloud+spring boot的依赖关系

--- src/main/java/net/xicp/chocolatedisco/gatewayweb/feign/AuthFeign.java
+++ src/main/java/net/xicp/chocolatedisco/gatewayweb/feign/AuthFeign.java
@@ -1,7 +1,7 @@
 package net.xicp.chocolatedisco.gatewayweb.feign;
 
 import net.xicp.chocolatedisco.gatewayweb.entity.SysUser;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
